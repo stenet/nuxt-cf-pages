af666c96e0d34ce2cf1c4d3b05db133b7ec3c2e7
Stefan Heim <stefan.heim@tip.co.at>
2023-04-17 14:53:44 +0200

    initial

---
+++ _worker.js
@@ -0,0 +1,12 @@
+﻿export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      return Response.json({
+        source: "api"
+      });
+    }   
+    
+    return env.ASSETS.fetch(request);
+  }
+}
---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const r = ref(await $fetch("/api/test"));
+
+const text = computed(() => {
+  if (!r.value) return "empty";
+  
+  return JSON.stringify(r.value);
+})
+</script>
+
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
